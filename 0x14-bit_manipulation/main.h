cfec31fa3e925fc5d0018bc82bd56c79459632ca
SAFAEELBAKKALY <bakkalysafae@gmail.com>
2023-07-06 16:17:28 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+
+int _putchar(char);
+unsigned int binary_to_uint(const char *b);
+
+
+
+
+
+
+
+
+
+#endif
